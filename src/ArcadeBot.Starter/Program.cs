6ded75a9110de1e9147f843da5673ccbd7cef568
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-06-05 02:09:59 +0200

    Removed old code

--- src/ArcadeBot.Starter/Program.cs
+++ src/ArcadeBot.Starter/Program.cs
@@ -1,5 +1,4 @@
 using ArcadeBot.Core;
-using ArcadeBot.Net.WebSockets;
 using Serilog;
 
 namespace ArcadeBot.Starter;
@@ -21,9 +20,8 @@ public class Program
             .ConfigureServices((ctx, services) =>
             {
                 services
-                .AddMediator()
-                .AddDiscordWebsockets(ctx.Configuration.GetSection("Bot"))
-                .AddHostedService<WebSocketWorker>();
+                .AddMediator();
+                // .AddDiscordWebsockets(ctx.Configuration.GetSection("Bot"));
                 // services.AddHostedService<WebSocketWorker>();
             })
             .Build();
